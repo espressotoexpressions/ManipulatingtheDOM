6409884d0b90f084c6186ea068f41ca1c3e43efd
espressotoexpressions <keannevillar@gmail.com>
2024-11-15 16:30:47 -0800

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,39 @@
+/*button 1 */
+
+let btn1Change = document.getElementById('btn1');
+
+let txt1Change = document.getElementById('txt1');
+
+btn1Change.addEventListener('click',function(event){
+txt1Change.innerText = "Surprise, This is a new message!";
+
+});
+
+/*button 2*/
+let btn2Change = document.getElementById('btn2');
+
+let txt2Change = document.getElementById('txt2');
+
+btn2Change.addEventListener('click',function(event){
+txt2Change.className = "colorgreen";
+
+});
+
+/*button 3*/
+let btn3Change = document.getElementById('btn3');
+
+let txt3Change = document.getElementById('txt3');
+
+btn3Change.addEventListener('click',function(event){
+txt3Change.className = "makebigger";
+
+});
+
+/*button 4*/
+let btn4Change = document.getElementById('btn4');
+
+let txt4Change = document.getElementById('txt4');
+let img4Change= document.getElementById('img4')
+btn3Change.addEventListener('click',function(event){
+
+});
